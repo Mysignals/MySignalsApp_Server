dd44361f492dc121dc098c34d8f3fc8b961c6d63
Godand <aezeigwe2003@gmail.com>
2023-04-24 19:00:29 +0000

    user activate| reset password |jwt helpers

--- Sig/user/routes.py
+++ Sig/user/routes.py
@@ -1,6 +1,6 @@
 from user.models import User
 from flask import jsonify, request, Blueprint, session
-from Sig.utils import query_one_filtered
+from Sig.utils import query_one_filtered, get_reset_token, verify_reset_token
 from sig import bcrypt
 
 
@@ -27,7 +27,9 @@ def register_user():
             304,
         )
 
-    if query_one_filtered(user_name=user_name) or query_one_filtered(email=email):
+    if query_one_filtered(User, user_name=user_name) or query_one_filtered(
+        User, email=email
+    ):
         return (
             jsonify(
                 {"error": "Conflict", "message": "User_name or email already exists"}
@@ -45,6 +47,28 @@ def register_user():
         {"message": "Success", "user_name": user.user_name, "email": user.email}, 200
     )
 
+@user.route("/activate/<string:token>")
+def activate_user(token):
+    user = verify_reset_token(User, token)
+    if user:
+        user.is_active = True
+        user.update()
+        return jsonify(
+            {
+                "message": "Success",
+                "user_name": user.user_name,
+                "is_active": user.is_active,
+            },
+            200,
+        )
+    return jsonify(
+        {
+            "error": "Unauthorized",
+            "message": "Token is not valid or has already been used",
+        },
+        404,
+    )
+
 
 @user.route("/login", methods=["POST"])
 def login_user():
@@ -57,9 +81,9 @@ def login_user():
             {"error": "Bad Request", "message": "Did you provide all fields?"}, 304
         )
 
-    user = query_one_filtered(user_name=user_name_or_mail)
+    user = query_one_filtered(User, user_name=user_name_or_mail)
     if not user or not bcrypt.check_password_hash(user.password, password):
-        user = query_one_filtered(email=user_name_or_mail)
+        user = query_one_filtered(User, email=user_name_or_mail)
         if not user or not bcrypt.check_password_hash(user.password, password):
             return jsonify(
                 {"error": "Unauthorized", "message": "Incorrect username or password"},
@@ -67,15 +91,63 @@ def login_user():
             )
 
         session["user"] = user.id
-        return jsonify({"message": "Success", "user_name": user.user_name}, 200)
+        return jsonify(
+            {
+                "message": "Success",
+                "user_name": user.user_name,
+                "is_active": user.is_active,
+            },
+            200,
+        )
     session["user"] = user.id
     return jsonify(
         {
             "message": "Success",
             "user_name": user.user_name,
+            "is_active": user.is_active,
         },
         200,
     )
+
+
+@user.route("/reset_password",methods=["POST"])
+def reset_request():
+    data=request.get_json()
+    email = data.get("email")
+    user= query_one_filtered(User,email)
+    if user:
+        token=get_reset_token(user)
+        # send_reset_password_email(user,token)
+        return jsonify({
+            "message": f"Reset password token sent to {email}"
+        },200)
+
+
+@user.route("/reset_password/<string:token>", methods=["POST"])
+def reset_token(token):
+    data=request.get_json()
+    password=data.get("password")
+    password_confirm=data.get("password_confirm")
+    if not password or not password_confirm:
+        return (
+            jsonify({"error": "Bad Request", "message": "Did you provide all fields?"}),
+            304,
+        )
+
+    if password != confirm_password:
+        return (
+            jsonify({"error": "Bad Request", "message": "Passwords do not match"}),
+            304,
+        )
+    user = verify_reset_token(User, token)
+    if user:
+        user.password=bcrypt.generate_password_hash(password)
+        user.update()
+        session.pop("user", None)
+        return jsonify({"message": "Password changed"},200)
+    return jsonify({"error": "Invalid token"},400)
+    
+
 
 
 @user.route("/logout", methods=["GET"])
--- Sig/utils.py
+++ Sig/utils.py
@@ -1,6 +1,9 @@
 from Sig import db
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
+# db helpers
 def query_one_filtered(table, **kwargs):
     return db.session.execute(db.select(table).filter_by(**kwargs)).scalar_one_or_none()
 
@@ -29,3 +32,18 @@ def query_paginate_filtered(table, page, **kwargs):
         per_page=5,
         page=page,
     )
+
+
+# jwt helpers
+def get_reset_token(user, expires_sec=1800):
+    s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
+    return s.dumps({"user_id": user.id}).decode("utf-8")
+
+
+def verify_reset_token(user, token):
+    s = Serializer(current_app.config["SECRET_KEY"])
+    try:
+        user_id = s.loads(token)["user_id"]
+    except:
+        return None
+    return query_one_filtered(User, id=user_id)
