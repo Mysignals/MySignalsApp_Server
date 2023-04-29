5e580be2cf2dadfa27c78ae9cc0341e0a881a530
Godand <aezeigwe2003@gmail.com>
2023-04-29 10:04:52 +0000

    add Signal models |update db

--- MySignalsApp/models.py
+++ MySignalsApp/models.py
@@ -1,6 +1,7 @@
 from MySignalsApp import db
 from uuid import uuid4
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import JSON
 import enum
 
 
@@ -18,6 +19,8 @@ class Roles(enum.Enum):
         return [c.value for c in Roles]
 
 
+
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(
@@ -30,6 +33,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), nullable=False, default=False)
     roles = db.Column(db.Enum(Roles), nullable=False, default=Roles.USER)
     date_registered = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    signals=db.Relationship("Signal",backref="provider",lazy=True)
 
     def __init__(self, user_name, email, password, roles=Roles.USER, api_key=None):
         self.user_name = user_name
@@ -51,3 +55,32 @@ class User(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+class Signal(db.Model):
+    __tablename__="signals"
+    id=db.Column(db.Integer(), primary_key=True,unique=True,nullable=False)
+    signal=db.Column(JSON,nullable=False)
+    status=db.Column(db.Boolean(),nullable=False,default=False)
+    provider=db.Column(db.String(34),db.ForeignKey("users.id"),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow,nullable=False)
+    
+
+    def __init__(self,signal,status,provider):
+        self.signal=signal
+        self.status=status
+        self.provider=provider
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"signal({self.signal}), status({self.status}), date_created({self.date_created}), provider({self.provider.user_name}))"
