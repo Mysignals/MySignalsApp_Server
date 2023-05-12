b681b643fc95be912d975de832e67566eed2157c
Godand <aezeigwe2003@gmail.com>
2023-05-12 05:35:58 -0400

    add UpdateKey,Spot,Login,ValidEmail Schemas

--- MySignalsApp/schemas.py
+++ MySignalsApp/schemas.py
@@ -14,9 +14,34 @@ class RegisterScheme(BaseModel):
         if len(v) > 345 :
             raise ValueError
         return v
-        
+
     @validator("confirm_password")
     def passwords_are_same(cls,v,values):
         if v != values["password"]:
             raise ValueError("Passwords do not match")
+        return v
+
+class LoginScheme(BaseModel):
+    user_name_or_mail:str
+    password:str
+
+class UpdateKeysSchema(BaseModel):
+    api_key:constr(max_length=100)
+    api_secret:constr(max_length=100)
+
+class ValidEmailSchema(BaseModel):
+    email:EmailStr
+
+class Spotschema(BaseModel):
+    symbol:constr(max_length=12)
+    side:constr(max_length=4)
+    quantity:float
+    price:float
+    sl:float
+    tp:float
+
+    @validator("side")
+    def is_buy_or_sell(cls,v):
+        if v not in ["BUY","SELL"]:
+            raise ValueError
         return v
