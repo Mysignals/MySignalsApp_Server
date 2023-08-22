690af73d415f9cc7576b4135df2c81d6c36e7e47
Godand <aezeigwe2003@gmail.com>
2023-08-22 22:06:53 +0000

    samesite none

--- MySignalsApp/config.py
+++ MySignalsApp/config.py
@@ -11,6 +11,7 @@ class App_Config:
     SESSION_USE_SIGNER = True
     SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = False
+    SESSION_COOKIE_SAMESITE = "None"
     PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=1)  # TODO EDIT to 1
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
